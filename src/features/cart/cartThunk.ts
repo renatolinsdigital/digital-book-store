6f4a50e3de3eaa83682391d3d56996c015b105d8
Renato Lins <renato.lins.digital@outlook.com>
2024-09-02 22:59:34 -0300

    updates fake data location

--- src/features/cart/cartThunk.ts
+++ src/features/cart/cartThunk.ts
@@ -6,7 +6,7 @@ export const fetchProducts = createAsyncThunk<Product[]>(
   'cart/fetchProducts',
   async () => {
     try {
-      const response = await axios.get('src/data/books.json');
+      const response = await axios.get('data/books.json');
       return response.data.products;
     } catch (error) {
       console.error(error);
